1b6c59fe60aa08a4463ef1c9a030c7c9039689d7
Alexander Christie <alex@briefly.io>
2016-04-10 21:54:53 +0100

    Fix the userconfig
    Signed-off-by: Alexander Christie <alex@briefly.io>

--- update-modpack.sh
+++ update-modpack.sh
@@ -3,3 +3,6 @@
 cd /c/server/arma
 
 rsync -avzhe ssh root@content.1st-rrf.com:/usr/share/nginx/html/modpack /c/server/arma
+
+rm -rf /c/server/arma/userconfig
+cp -r /c/server/arma/modpack/userconfig /c/server/arma/userconfig
